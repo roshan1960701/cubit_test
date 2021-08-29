3a4b6a3b33d7350d032a42d2766833288551c30f
ROSHAN <roshanw1998@gmail.com>
2021-08-29 23:05:15 +0530

    observer methods added

--- lib/counter_observer.dart
+++ lib/counter_observer.dart
@@ -5,18 +5,27 @@ class CounterOberserver extends BlocObserver{
   @override
   void onChange(Cubit cubit, Change change) {
     super.onChange(cubit, change);
+    print("OnChange");
     print("${cubit.runtimeType} $change");
   }
 
   @override
   void onEvent(Bloc bloc, Object event) {
     super.onEvent(bloc, event);
+    print("OnEvent");
     print("${bloc.runtimeType} $event");
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
+    print("OnTransition");
     print("${bloc.runtimeType} $transition");
+  }
+
+  @override
+  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
+    super.onError(cubit, error, stackTrace);
+    print("$cubit $error $stackTrace ");
   }
 }
